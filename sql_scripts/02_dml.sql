dbad32d3d6da058aa485aa9611728dd2d03803bf
ZendorXX <prime.zen2017@yandex.ru>
2025-05-19 20:04:55 +0300

    Add docker-compose and sql scripts for loading source data

---
+++ sql_scripts/02_dml.sql
@@ -0,0 +1,21 @@
+-- DML: Импорт данных из всех CSV-файлов
+COPY mock_data
+FROM '/mock_data/MOCK_DATA.csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (1).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (2).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (3).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (4).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (5).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (6).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (7).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (8).csv' WITH CSV HEADER;
+COPY mock_data
+FROM '/mock_data/MOCK_DATA (9).csv' WITH CSV HEADER;
